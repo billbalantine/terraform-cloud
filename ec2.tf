fcf6be0e6378a94f0b426c4a657df0cab5485d44
billbalantine <61770047+billbalantine@users.noreply.github.com>
2020-09-16 10:26:06 +0200

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0083662ba17882949"
+  instance_type = "t2.micro"
+  key_name      = "devops-alain"
+  tags = {
+    Name = "ec2-alain"
+  }
+
+}
